4c8736102a25d340613f43053e51f60d87a39d79
FactoryBlack <43464404+FactoryBlack@users.noreply.github.com>
2024-10-14 07:54:41 +1000

    Update entrypoint.sh

--- trackmania/2020/entrypoint.sh
+++ trackmania/2020/entrypoint.sh
@@ -2,7 +2,7 @@
 echo ">>> Environment setup starting..."
 
 # Fix permissions if necessary
-chown -R container:container /mnt/server
+chown -R 0:0 /mnt/server
 
 # Ensure we're in the correct directory
 cd /mnt/server
