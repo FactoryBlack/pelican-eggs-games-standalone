d74cecfe949d99c2f8c9b7f246ad4cab572b5670
FactoryBlack <43464404+FactoryBlack@users.noreply.github.com>
2024-10-14 12:35:32 +1000

    Update entrypoint.sh

--- trackmania/2020/entrypoint.sh
+++ trackmania/2020/entrypoint.sh
@@ -12,6 +12,6 @@ echo "Current directory: $(pwd)"
 ls -la /mnt/server
 
 # Start the server (if the egg doesn't handle it)
-sudo ./start-minicontrol.sh
+./start-minicontrol.sh
 
 echo ">>> Environment setup complete. Server starting..."
