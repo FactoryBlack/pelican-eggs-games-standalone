e5fe35839f669da55709d6163b7f8872ceff3569
FactoryBlack <43464404+FactoryBlack@users.noreply.github.com>
2024-10-14 17:10:39 +1000

    Update start-minicontrol.sh

--- trackmania/2020/start-minicontrol.sh
+++ trackmania/2020/start-minicontrol.sh
@@ -1,10 +1,11 @@
 #!/bin/ash
+
 echo ">>> Starting Trackmania server..."
-cd /mnt/server
+cd /home/container/server || exit 1
 ./TrackmaniaServer /title=Trackmania /game_settings=MatchSettings/tracklist.txt /dedicated_cfg=dedicated_cfg.txt /nodaemon &
 
 echo ">>> Starting MiniControl..."
-cd /home/container/minicontrol
+cd /home/container/minicontrol || exit 1
 tsx --env-file=.env core/minicontrol.ts &
 
 minicontrol_pid=$!
@@ -12,15 +13,15 @@ minicontrol_pid=$!
 echo ">>> Both Trackmania and MiniControl are running. Type '!stop' to stop the servers."
 
 while [[ $INPUT != "!stop" ]]; do
-    eval $INPUT
-    read INPUT
+    eval "$INPUT"
+    read -r INPUT
 done
 
 echo ">>> Stopping Trackmania server..."
 trackmania_pid=$(pgrep -f ./TrackmaniaServer)
-kill $trackmania_pid
+kill "$trackmania_pid"
 
 echo ">>> Stopping MiniControl..."
-kill $minicontrol_pid
+kill "$minicontrol_pid"
 
 echo ">>> Both servers stopped."
